c493ff00f8e6eabd959a6f4ddf31c6cd4fea1b92
pragyes31 <rahulnayak@electroons.com>
2020-05-28 20:42:05 +0530

    initial setup commit

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./styles.scss"
+import AppDashboard from "./Components/AppDashboard";
+
+export default function App() {
+  return (
+    <div className="go-dutch-app">
+      <AppDashboard />
+    </div>
+  );
+}
--- index.js
+++ index.js
@@ -1,26 +1,11 @@
-import React, { Component } from 'react';
-import { render } from 'react-dom';
-import Hello from './Hello';
-import './style.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      name: 'React'
-    };
-  }
-
-  render() {
-    return (
-      <div>
-        <Hello name={this.state.name} />
-        <p>
-          Start editing to see some magic happen :)
-        </p>
-      </div>
-    );
-  }
-}
-
-render(<App />, document.getElementById('root'));
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  rootElement
+);
