843f131b87aadcde38fcdfbc79d4beebc4a8f88d
pragyes31 <rahulnayak@electroons.com>
2020-05-28 21:06:59 +0530

    Header component added

--- Components/AppDashboard.js
+++ Components/AppDashboard.js
@@ -1,11 +1,29 @@
 import React from "react";
 
+function Header() {
+  return (
+    <div className="app-header">
+      <div className="hamburger-icon">
+        <div className="hamBurger-line" />
+        <div className="hamBurger-line" />
+        <div className="hamBurger-line" />
+      </div>
+      <div className="header-title">Go-Dutch App</div>
+      <div className="options">3-dots-options</div>
+    </div>
+  );
+}
+
 export default class AppDashboard extends React.Component {
   constructor(props) {
     super(props);
   }
 
   render() {
-    return <div className="app-dashboard">App goes here!</div>;
+    return (
+      <div className="app-dashboard">
+        <Header />
+      </div>
+    );
   }
 }
