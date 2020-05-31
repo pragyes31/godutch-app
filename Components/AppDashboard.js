6cb4d7e179f21bfde3624c4b7ff769d7fc9d51e7
pragyes31 <rahulnayak@electroons.com>
2020-05-31 18:49:47 +0530

    2 files modified

--- Components/AppDashboard.js
+++ Components/AppDashboard.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-function Header() {
+function Header(props) {
   return (
     <div className="app-header">
       <div className="hamburger-icon">
@@ -9,7 +9,7 @@ function Header() {
         <div className="hamBurger-line" />
       </div>
       <div className="header-title">Go-Dutch App</div>
-      <div className="options" />
+      <div className="options" onClick={props.toggleModal("optinsModal")} />
     </div>
   );
 }
@@ -17,12 +17,17 @@ function Header() {
 export default class AppDashboard extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      optionsModal: false
+    };
   }
+
+  toggleModal = () => {};
 
   render() {
     return (
       <div className="app-dashboard">
-        <Header />
+        <Header toggleModal={this.toggleModal} />
       </div>
     );
   }
