381e0f1f67ea834afa3a37ace3fa56a19883650d
pragyes31 <rahulnayak@electroons.com>
2020-06-01 21:07:09 +0530

    hamburger icon modal added

--- Components/AppDashboard.js
+++ Components/AppDashboard.js
@@ -1,21 +1,36 @@
 import React from "react";
+import { Modal } from "@material-ui/core";
 
 function Header(props) {
   return (
     <div className="app-header">
-      <div className="hamburger-icon">
+      <div
+        className="hamburger-icon"
+        onClick={() => props.toggleModal("optionsModal")}
+      >
         <div className="hamBurger-line" />
         <div className="hamBurger-line" />
         <div className="hamBurger-line" />
       </div>
       <div className="header-title">Go-Dutch App</div>
-      <div className="options" onClick={() => props.toggleModal("optionsModal")} />
+      <div className="options" />
     </div>
   );
 }
 
 function OptionsModal(props) {
-  return <div />;
+  return (
+    <div>
+      <Modal
+        open={props.optionsModal}
+        aria-labelledby="simple-modal-title"
+        aria-describedby="simple-modal-description"
+        onBackdropClick={() => props.toggleModal("optionsModal")}
+      >
+        <div>Modal window</div>
+      </Modal>
+    </div>
+  );
 }
 
 export default class AppDashboard extends React.Component {
@@ -26,15 +41,18 @@ export default class AppDashboard extends React.Component {
     };
   }
 
-  toggleModal = (modal) => {
-    this.setState({optionsModal:!this.state.optionsModal})
+  toggleModal = modal => {
+    this.setState({ optionsModal: !this.state.optionsModal });
   };
 
   render() {
     return (
       <div className="app-dashboard">
         <Header toggleModal={this.toggleModal} />
-        {this.state.optionsModal && <OptionsModal />}
+        <OptionsModal
+          optionsModal={this.state.optionsModal}
+          toggleModal={this.toggleModal}
+        />
       </div>
     );
   }
