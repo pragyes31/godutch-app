a1cb568ae6a511585d652a78f127aceb6c466a24
pragyes31 <rahulnayak@electroons.com>
2020-06-04 21:48:09 +0530

    NavBar component created

--- Components/AppDashboard.js
+++ Components/AppDashboard.js
@@ -65,7 +65,13 @@ function ThreeDotsModal(props) {
 }
 
 function NavBar(props) {
-  return <div className="nav-bar" />;
+  return (
+    <div className="nav-bar">
+      <div className="nav-bar__friends nav-bar--items">friends</div>
+      <div className="nav-bar__groups nav-bar--items">groups</div>
+      <div className="nav-bar__activity nav-bar--items">activity</div>
+    </div>
+  );
 }
 
 function AddExpense(props) {
@@ -113,6 +119,7 @@ export default class AppDashboard extends React.Component {
           optionsModal={this.state.optionsModal}
           toggleModal={this.toggleModal}
         />
+        <NavBar />
         <AddExpense addExpense={this.addExpense} />
       </div>
     );
